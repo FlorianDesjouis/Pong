72711510f5d2c961d1c08296c62f5d2af4f2ea0f
FlorianDesjouis <desjouisflorian@gmail.com>
2018-03-28 23:14:49 +0200

    pong

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -13,98 +13,57 @@ class GameScene: SKScene {
     
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
+    private var computerScore = 0
+    private var playerScore = 0
+    var elements = [String: SKSpriteNode]()
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView) {
+        elements["ball"] = self.childNode(withName: "ball") as! SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        elements["ball"]?.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 70))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let frameBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        frameBody.friction = 0
+        frameBody.restitution = 1
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        self.physicsBody = frameBody
     }
+    
+    func touchMoved(toPoint pos: CGPoint){
+        self.childNode(withName: "playerBottom")?.position.x = pos.x
+    }
+    
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+//         Called before each frame is rendered
+        self.childNode(withName: "playerTop")?.run(SKAction.moveTo(x: (self.childNode(withName: "ball")?.position.x)!, duration: 0.15 ))
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        if((self.childNode(withName: "ball")?.position.y)! > CGFloat(565)){
+            self.playerScore += 1
+            self.childNode(withName: "ball")?.position.x = 0
+            self.childNode(withName: "ball")?.position.y = 0
+            elements["ball"]?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            elements["ball"]?.physicsBody?.applyImpulse(CGVector(dx: -50, dy: -50))
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
+        if((self.childNode(withName: "ball")?.position.y)! < CGFloat(-565)){
+            self.computerScore += 1
+            self.childNode(withName: "ball")?.position.x = 0
+            self.childNode(withName: "ball")?.position.y = 0
+            elements["ball"]?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            elements["ball"]?.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
         }
         
-        self.lastUpdateTime = currentTime
+        let computerScoreLabel = self.childNode(withName: "computerScore") as! SKLabelNode
+        let playerScoreLabel = self.childNode(withName: "playerScore") as! SKLabelNode
+        
+        computerScoreLabel.text = "\(self.computerScore)"
+        playerScoreLabel.text = "\(self.playerScore)"
+        
     }
 }
---
+++ Pong/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  viewController.swift
+//  Pong
+//
+//  Created by Florian Desjouis on 19/03/2018.
+//  Copyright © 2018 Florian Desjouis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    @IBAction func PlayComputer(_ sender: UIButton) {
+//        performSegue(withIdentifier: , sender: self)
+//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
